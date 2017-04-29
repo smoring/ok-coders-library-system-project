37bb0dc961eced8e52cfc80ed87a6607e6b86e01
Susan Moring <susanmoring@gmail.com>
2017-04-29 18:10:27 -0500

    updated users schema

--- models/users.js
+++ models/users.js
@@ -2,7 +2,6 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var schema = new Schema({	
-// User collection schema
 	objectID: Schema.Types.ObjectId,
 	firstName: String,
 	lastName: String,
